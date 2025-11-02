f1f163281e5e9d6bd8676bc6f9627ab67674ed51
thomasyang4096 <58776244+thomasyang4096@users.noreply.github.com>
2025-11-03 07:59:59 +0800

    Update 3dview.vb

--- 3dview.vb
+++ 3dview.vb
@@ -24,7 +24,33 @@ Partial Class three3dview
                       .url = "https://example.com/c"}
         }
 
+                                            ' === 建立文字標籤資料 ===
+        Dim labels As New List(Of Object)
+
+        labels.Add(New With {
+            .text = "入口",
+            .pos = New With {.x = 0, .y = 3, .z = 0},
+            .color = "#00FF00",
+            .size = 64
+        })
+        labels.Add(New With {
+            .text = "電梯",
+            .pos = New With {.x = 3, .y = 2.5, .z = 1},
+            .color = "#FFFF00",
+            .size = 48
+        })
+        labels.Add(New With {
+            .text = "出口",
+            .pos = New With {.x = -2, .y = 3, .z = -3},
+            .color = "#FF5555",
+            .size = 72
+        })
+
+        ' === 序列化成 JSON 給前端 ===
         Dim js As New JavaScriptSerializer()
         ArrowJson = js.Serialize(arrows)
+        LabelJson = js.Serialize(labels)
+
+     
     End Sub
 End Class
